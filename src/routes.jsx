76c6fdaaf740bcda9b08d555e8207f546e7b4ee3
yessigarridowork-blip <yessigarrido.work@gmail.com>
2025-12-10 14:27:50 +0000

    mi lista de conactos

--- src/routes.jsx
+++ src/routes.jsx
@@ -27,7 +27,7 @@ export const router = createBrowserRouter(
         <Route path= "/home" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
-        <Route path="/contactos" element={<Contactos />} />
+        
       </Route>
     )
 );
